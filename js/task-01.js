58e613223ea2ed65c69155a1e62745a5cc68d301
MarinaMuntian <dp060491smv@gmail.com>
2021-06-05 17:09:30 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -4,6 +4,6 @@ console.log(categoriesQuantity());
 
 const categories = document.querySelectorAll('h2');
 const categoriesName = categories.forEach(category => console.log(`Категория: ${category.textContent}`));
-const quantity = document.querySelector('.item').lastElementChild.children;
-const elQuantity = quantity.forEach(el => console.log(`Количество элементов: ${ el.length }`));
 
+const quantity = document.querySelectorAll('li.item > ul');
+const elQuantity = quantity.forEach(el => console.log(`Количество элементов: ${el.children.length}`));
