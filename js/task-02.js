5dc5822130d984d14a7c3599fed70cdf0f5dd504
MarinaMuntian <dp060491smv@gmail.com>
2021-06-10 13:59:17 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,4 +1,5 @@
-// Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li, после чего вставит все li за одну операцию в список ul.ingredients. Для создания DOM-узлов используй document.createElement().
+// Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li, 
+// после чего вставит все li за одну операцию в список ul.ingredients. Для создания DOM - узлов используй document.createElement().
 
 const ingredients = [
   'Картошка',
@@ -9,8 +10,20 @@ const ingredients = [
   'Приправы',
 ];
 
-const list = ingredients.forEach(ingredient => {
-  let item = document.createElement('li');
-  item.textContent = ingredient;  
-document.getElementById('ingredients').append(item);
-});
+// const list = ingredients.forEach(ingredient => {
+//   let item = document.createElement('li');
+//   item.textContent = ingredient;  
+// document.getElementById('ingredients').append(item);
+// });
+
+const addList = (ingredients) => {
+  const ingredientsList = ingredients.map(ingredient => {
+    const item = document.createElement('li');
+    item.textContent = ingredient;
+    return item;
+  })
+
+  document.getElementById('ingredients').append(...ingredientsList);
+}
+
+addList(ingredients);
